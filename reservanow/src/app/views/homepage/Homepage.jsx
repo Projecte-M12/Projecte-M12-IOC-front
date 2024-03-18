6b91d3c76712cfa16913927f4f49d6b7da5579ce
oarnal <oarnal@gmail.com>
2024-03-18 19:59:00 +0100

    Update. Cambios en el css y modificaciones en componentes

--- reservanow/src/app/views/homepage/Homepage.jsx
+++ reservanow/src/app/views/homepage/Homepage.jsx
@@ -1,10 +1,15 @@
+import { Header } from "../../shared/components/Header";
+import { Footer } from "../../shared/components/Footer";
 
 export const Homepage = () => {
-
   return (
     <div>
-      <h1>Homepage</h1>
-      <p>Welcome to ReservaNOW</p>
+      <Header />
+      <div>
+        <h1>Homepage</h1>
+        <p>Welcome to ReservaNOW</p>
+      </div>
+      <Footer />
     </div>
   );
 };
