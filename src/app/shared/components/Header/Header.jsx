9e6cc4ce55951d07a2ea2293adba6927ba93de1e
oarnal <oarnal@gmail.com>
2024-04-16 02:09:04 +0200

    Update. Prova d'estat amb el component Header

--- src/app/shared/components/Header/Header.jsx
+++ src/app/shared/components/Header/Header.jsx
@@ -1,9 +1,17 @@
-import "./Header.scss";
+import { useAuthContext } from '../../../hooks/useAuthContext';
+import './Header.scss';
 
 export const Header = () => {
-  return (
-    <header className="header">
-      <p>Header</p>
-    </header>
-  );
+    const { isAuthenticated } = useAuthContext();
+
+    return (
+        <header className="header">
+            <div>
+                <p>Header</p>
+            </div>
+            <div>
+                {isAuthenticated ? <p>Botó Logout</p> : <p>Botó Login</p>}
+            </div>
+        </header>
+    );
 };
