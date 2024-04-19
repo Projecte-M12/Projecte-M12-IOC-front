74347bc81111279c4d7ab89ee212a6a7aad9913f
oarnal <oarnal@gmail.com>
2024-04-20 01:48:50 +0200

    Update. Replace en el link de proves per tornar al Login

--- src/app/router/ProtectedRoutes.jsx
+++ src/app/router/ProtectedRoutes.jsx
@@ -3,5 +3,5 @@ import { useAuthContext } from '../hooks/useAuthContext';
 
 export function ProtectedRoutes() {
   const auth = useAuthContext();
-  return auth.isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
+  return auth.isAuthenticated ? <Outlet /> : <Navigate to="/login" replace/>;
 }
