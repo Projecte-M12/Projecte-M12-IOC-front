a725bf6e63ff256bc5b99e30d635e963ee3bc784
oarnal <oarnal@gmail.com>
2024-04-23 00:18:30 +0200

    Update. Arreglat problema del render d'un button dins d'un button i els estils perquè quedin iguals

--- src/app/shared/components/InvertedButton/InvertedButton.jsx
+++ src/app/shared/components/InvertedButton/InvertedButton.jsx
@@ -2,19 +2,38 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-// Estils CSS 
+// Estils CSS
 import './InvertedButton.css';
 
 // Component del botó primari; pots passar; una url una acció o un text
-export function InvertedButton({ text, url, action, isLink, className = "inverted__primary-button" }) {
-    const ButtonComponent = isLink ? Link : 'button';
+export function InvertedButton({
+    text,
+    url,
+    action,
+    isLink,
+    className = 'inverted__primary-button',
+}) {
+    // const ButtonComponent = isLink ? Link : 'button';
 
-    return (
-        <ButtonComponent to={url} onClick={action}>
-            <button className={className}>
+    if (isLink) {
+        return (
+            <Link to={url} className={className}>
+                <p className="inverted-text">{text}</p>
+            </Link>
+        );
+    } else {
+        return (
+            <button className={className} onClick={action}>
                 <p className="inverted-text">{text}</p>
             </button>
-        </ButtonComponent>
-    );
-}
+        );
+    }
 
+    // return (
+    //     <ButtonComponent to={url} onClick={action}>
+    //         <button className={className}>
+    //             <p className="inverted-text">{text}</p>
+    //         </button>
+    //     </ButtonComponent>
+    // );
+}
