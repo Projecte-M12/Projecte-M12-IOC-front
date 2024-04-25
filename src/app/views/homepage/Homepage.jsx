8b31e51816805589b4f2cee6d2e4be172be36c1f
oarnal <oarnal@gmail.com>
2024-04-25 23:49:31 +0200

    Update. Actualització Homepage i Card

--- src/app/views/homepage/Homepage.jsx
+++ src/app/views/homepage/Homepage.jsx
@@ -1,24 +1,45 @@
 import { Header } from '../../shared/components/Header/Header';
 import { Footer } from '../../shared/components/Footer/Footer';
 import { Card } from './components/card/Card';
-
-import './Homepage.css';
+import { getCompanies } from '../../services/getCompanies';
+// import Modal from 'react-modal';
+import './Homepage.scss';
+import { useState } from 'react';
+import CustomModal from './components/modal/Modal';
 
 // import {useGetImage} from "../../hooks/customHooks/useGetImage";
 
+
 export const Homepage = () => {
+    const [selectedCompany, setSelectedCompany] = useState(null);
+    const companies = getCompanies();
+
+    const openModal = (company) => {
+        setSelectedCompany(company);
+    };
+
+    const closeModal = () => {
+        setSelectedCompany(null);
+    };
     return (
         <>
             <Header />
             <h1>Homepage</h1>
             <p className="homepage__title">Welcome to ReservaNOW</p>
 
-            {
+            <div className="homepage__container">
                 <ul>
-                    <Card />
+                    {companies.map((company) => {
+                        return (
+                            <li key={company.companyId} className="card">
+                                <Card company={company} openModal={openModal} />
+                            </li>
+                        );
+                    })}
                 </ul>
-            }
+            </div>
             <Footer />
+            <CustomModal company={selectedCompany} closeModal={closeModal} />
         </>
     );
 };
--- src/app/views/homepage/components/card/Card.jsx
+++ src/app/views/homepage/components/card/Card.jsx
@@ -1,26 +1,17 @@
-import { getCompanies } from "../../../../services/getCompanies";
-import "./Card.scss";
+// import { getCompanies } from '../../../../services/getCompanies';
+import './Card.scss';
 
-export const Card = () => {
-  //   const { company } = useFilterCompanyById(companyId);
-  const companies = getCompanies();
-  return (
-    <>
-      <div className="card">
-        <ul>
-          {companies.map((company) => {
-            return (
-              <li key={company.companyId}>
-                <p>Company: {company.companyName}</p>
-                <div className="card__image">
-                  <img src={company.srcImages.small} alt="" />
-                </div>
-                <p>Category: {company.category}</p>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-    </>
-  );
+export const Card = ({ company, openModal }) => {
+    const handleClick = () => {
+        openModal(company);
+    };
+    return (
+        <>
+            <p>Company: {company.companyName}</p>
+            <div className="card__image" onClick={handleClick}>
+                <img src={company.srcImages.small} alt="" />
+            </div>
+            <p>Category: {company.category}</p>
+        </>
+    );
 };
