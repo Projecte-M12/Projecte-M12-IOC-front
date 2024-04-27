127b67bec4954c260a516411822c26060ec6c97b
oarnal <oarnal@gmail.com>
2024-04-27 23:52:57 +0200

    Update. Reservas desde homepage funcionando. Falta enlace con DB

--- src/app/context/AppointmentsContextProvider.jsx
+++ src/app/context/AppointmentsContextProvider.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { appointmentsContext } from './appointmentsContext';
 import { useState } from 'react';
 
@@ -5,19 +6,53 @@ export function AppointmentsContextProvider({ children }) {
     /**
      * States
      */
-    const [appointments, setAppointments] = useState([]);
+    const [allAppointments, setAllAppointments] = useState([]);
+    // const [customerAppointments, setCustomerAppointments] = useState([]);
+    // const [providerAppointments, setProviderAppointments] = useState([]);
+    const [lastId, setLastId] = useState(0);
+
+    useEffect(() => {
+        // TODO: Llamada a la API para obtener las reservas de todos los clientes y proveedores.
+        getAllAppointments();
+
+        // TODO: Obtener el último id de las reservas en la BD y asignarlo a lastId.
+        const highestId = allAppointments.reduce((maxId, currentItem) => {
+            return Math.max(maxId, currentItem.id);
+        }, 0);
+        setLastId(highestId);
+
+    }, []);
 
     /**
      * Functions
      */
-    const updateApointments = (appointments) => setAppointments(appointments);
+    const updateAllApointments = (appointments) => {
+        setAllAppointments(appointments);
+        // TODO: Llamada a la API para actualizar las reservas
+    };
+    // const updateCustomerAppointments = (appointments) => {
+    //     setCustomerAppointments(appointments);
+    // };
+    // const updateProviderAppointments = (appointments) => {
+    //     setProviderAppointments(appointments);
+    // };
+
+    const getAllAppointments=() => {
+        // TODO: Llamada a la API para obtener las reservas
+        return []
+    }
 
     /**
      * RETURN
      */
     return (
         <appointmentsContext.Provider
-            value={{ appointments, updateApointments }}
+            value={{
+                lastId,
+                setLastId,
+                allAppointments,
+                updateAllApointments,
+            }}
         >
             {children}
         </appointmentsContext.Provider>
--- src/app/views/homepage/components/modal/Modal.jsx
+++ src/app/views/homepage/components/modal/Modal.jsx
@@ -2,66 +2,126 @@ import Modal from 'react-modal';
 import './Modal.scss';
 Modal.setAppElement('#root');
 
-import { Calendar, momentLocalizer } from 'react-big-calendar';
+import { Calendar, momentLocalizer, Navigate } from 'react-big-calendar';
 import moment from 'moment';
 import 'moment/locale/es'; // Importa el locale español
 import withDragAndDrop from 'react-big-calendar/lib/addons/dragAndDrop';
+
 import { useState } from 'react';
 import { useAuthContext } from '../../../../hooks/useAuthContext';
-import { useEventCalendar } from '../../../../hooks/useEventCalendar';
+import { useAppointmentsContext } from '../../../../hooks/useAppointmentsContext';
+import { useCheckUser } from '../../../../hooks/useCheckUser';
+import { useEffect } from 'react';
 
-moment.locale('es', {
-    week: {
-        dow: 1, // Lunes es el primer día de la semana
-    },
-});
+// moment().locale('es', {
+//     week: {
+//         dow: 1, // Lunes es el primer día de la semana
+//     },
+// });
 
 const localizer = momentLocalizer(moment);
 
 const CustomModal = ({ company, closeModal }) => {
+    // const [myEvent, setMyEvent] = useState([]);
+    const [newAppointments, setNewAppointments] = useState([]);
+    const [customerAppointments, setCustomerAppointments] = useState([]);
+    const [providerAppointments, setProviderAppointments] = useState([]);
+    const [visualAppointments, setVisualAppointments] = useState([]);
+    const [error, setError] = useState(null);
+
     const { user, isAuthenticated } = useAuthContext();
+    const { lastId, setLastId, allAppointments, updateAllApointments } =
+        useAppointmentsContext();
+    const { checkToken } = useCheckUser();
 
-    const [myEvent, setMyEvent] = useState([]);
-    const [companyEventsList, setCompanyEventsList] = useState([]);
+    useEffect(() => {
+        moment.locale('es', {
+            week: {
+                dow: 1, // Lunes es el primer día de la semana
+            },
+        });
+    }, []);
 
-    const { consoleDebug } = useEventCalendar();
+    useEffect(() => {
+        checkToken();
+    }, []);
 
-    let event = [];
-    let companyEvents = [];
+    useEffect(() => {
+        setCustomerAppointments(
+            allAppointments.filter(
+                (appointment) => appointment.userId === user?.id,
+            ),
+        );
+    }, [allAppointments, user?.id]);
+
+    useEffect(() => {
+        setProviderAppointments(
+            allAppointments.filter(
+                (appointment) => appointment.companyId === company?.companyId,
+            ),
+        );
+    }, [allAppointments, company?.companyId]);
+
+    useEffect(() => {
+        const mergedAppointments = [
+            ...customerAppointments,
+            ...providerAppointments,
+        ];
+        const filteredAppointments = Array.from(
+            new Set(mergedAppointments.map((item) => item.id)),
+        ).map((id) => {
+            return mergedAppointments.find((item) => item.id === id);
+        });
+        setVisualAppointments(filteredAppointments);
+    }, [customerAppointments, providerAppointments]);
+
+    useEffect(() => {
+        console.log('allAppointments', allAppointments);
+        console.log('providerAppointments', providerAppointments);
+        console.log('customerAppointments', customerAppointments);
+        console.log('visualAppointments', visualAppointments);
+    }, [providerAppointments, customerAppointments, allAppointments]);
+
+    if (!isAuthenticated) {
+        return <Navigate to="/" replace />;
+    }
 
     const handleSelectSlot = (slotInfo) => {
-        const reserva = {
-            id: 1,
+        const newAppointment = {
+            id: lastId + 1,
             companyId: company.companyId,
+            userId: user?.id,
             start: slotInfo.start,
             end: slotInfo.end,
             title: `Reserva en ${company.companyName}`,
             empresa: company.companyName,
-            email: `${user?.email}`,
-            color: '#FF5733',
+            email: user?.email,
+            color: '#66ff99', // Color para las nuevas reservas
             description: '', //Crear estado y campo input para manejar la descripción de la reserva
-            personas: 1, //Crear estado y campo input para manejar la descripción de la reserva,
         };
-        event = [...myEvent, reserva];
-        companyEvents = [...companyEventsList, reserva];
-        setMyEvent(event);
-        setCompanyEventsList(companyEvents);
 
-        consoleDebug(myEvent);
-        // console.log('Reserva', event);
-        // console.log('companyEventsList', companyEventsList);
+        setLastId(newAppointment.id);
+        updateAllApointments([...allAppointments, newAppointment]);
     };
 
     const handleSelectEvent = (eventInfo) => {
         //Elimina el evento pasado por parámetro
-        const updatedEvents = companyEventsList.filter(
-            (ev) => ev.id !== eventInfo.id,
-        );
-        setCompanyEventsList(updatedEvents);
-        setMyEvent([]);
+        if (eventInfo.userId === user?.id) {
+            updateAllApointments(
+                allAppointments.filter(
+                    (appointment) => appointment.id !== eventInfo.id,
+                ),
+            );
+        }
+        setError(null);
         console.log('eventInfo', eventInfo);
-        console.log(event);
     };
+
+    const minTime = new Date();
+    minTime.setHours(8, 0, 0);
+
+    const maxTime = new Date();
+    maxTime.setHours(18, 0, 0);
 
     return (
         <Modal
@@ -81,7 +141,81 @@ const CustomModal = ({ company, closeModal }) => {
                     </div>
                     <p>Nom: {company.companyName}</p>
                     <p>Categoria: {company.category}</p>
-                    {myEvent && (
+
+                    <div className="dashboard__calendar">
+                        <Calendar
+                            localizer={localizer}
+                            events={visualAppointments}
+                            defaultView={['day']}
+                            defaultDate={new Date()}
+                            // defaultView="month"
+                            startAccessor="start"
+                            endAccessor="end"
+                            style={{ height: 400 }}
+                            onSelectSlot={handleSelectSlot}
+                            onSelectEvent={handleSelectEvent}
+                            slotDuration={60}
+                            step={60}
+                            timeslots={1}
+                            min={minTime}
+                            max={maxTime}
+                            culture="es"
+                            toolbar={true}
+                            formats={{
+                                timeGutterFormat: 'HH:mm',
+                                dayFormat: (date, culture, localizer) =>
+                                    localizer.format(date, 'dddd', culture), // Formato de los días en la vista de semana
+                                agendaTodayLabel: {
+                                    long: 'Hoy', // Etiqueta para la opción "Hoy" en la vista de agenda
+                                    short: 'Hoy', // Etiqueta corta para la opción "Hoy" en la vista de agenda
+                                },
+                            }}
+                            eventPropGetter={(
+                                event,
+                                start,
+                                end,
+                                isSelected,
+                            ) => {
+                                if (
+                                    !customerAppointments.find(
+                                        (appointment) =>
+                                            appointment.id === event.id,
+                                    )
+                                ) {
+                                    // Reservas del cleinte
+                                    return {
+                                        style: {
+                                            backgroundColor: 'grey',
+                                            cursor: 'default',
+                                        },
+                                    };
+                                } else {
+                                    // Reservas nuevas
+                                    return {
+                                        style: {
+                                            backgroundColor: 'green',
+                                            cursor: 'pointer',
+                                        },
+                                    };
+                                }
+                            }}
+                            //Permite la edición de nuevas reservas
+                            editable={(event) =>
+                                customerAppointments.find(
+                                    (appointment) =>
+                                        appointment.id === event.id,
+                                )
+                            }
+                            //Evita la edición de reservas existentes
+                            selectable={(event) =>
+                                customerAppointments.find(
+                                    (appointment) =>
+                                        appointment.id === event.id,
+                                )
+                            }
+                        />
+                    </div>
+                    {/* {newAppointments.length > 0 && (
                         <>
                             <div className="modal__appointmentDatail">
                                 <div className="modal__appointmentDatail__header">
@@ -90,7 +224,7 @@ const CustomModal = ({ company, closeModal }) => {
                                 <p>
                                     Data i hora:{' '}
                                     <strong>
-                                        {myEvent.map((e) =>
+                                        {newAppointments.map((e) =>
                                             new Date(e.start).toLocaleString(),
                                         )}
                                     </strong>
@@ -98,7 +232,7 @@ const CustomModal = ({ company, closeModal }) => {
                                 <p>
                                     Temps:{' '}
                                     <strong>
-                                        {myEvent.map((e) => {
+                                        {newAppointments.map((e) => {
                                             const time = moment
                                                 .duration(e.end - e.start)
                                                 .asMinutes();
@@ -113,37 +247,14 @@ const CustomModal = ({ company, closeModal }) => {
                                 Info: Fes click sobre una reserva per
                                 eliminar-la
                             </div>
+                            <div
+                                className="modal__error"
+                                title="Per modificar la reserva, elimina l'anterior"
+                            >
+                                {error && <p>{error}</p>}
+                            </div>
                         </>
-                    )}
-                    <div className="dashboard__calendar">
-                        <Calendar
-                            localizer={localizer}
-                            events={companyEventsList}
-                            defaultView={['day']}
-                            defaultDate={new Date()}
-                            // defaultView="month"
-                            startAccessor="start"
-                            endAccessor="end"
-                            style={{ height: 600 }}
-                            selectable={true}
-                            onSelectSlot={handleSelectSlot}
-                            onSelectEvent={handleSelectEvent}
-                            slotDuration={60}
-                            step={60}
-                            timeslots={1}
-                            culture="es"
-                            toolbar={true}
-                            formats={{
-                                timeGutterFormat: 'HH:mm',
-                                dayFormat: (date, culture, localizer) =>
-                                    localizer.format(date, 'dddd', culture), // Formato de los días en la vista de semana
-                                agendaTodayLabel: {
-                                    long: 'Hoy', // Etiqueta para la opción "Hoy" en la vista de agenda
-                                    short: 'Hoy', // Etiqueta corta para la opción "Hoy" en la vista de agenda
-                                },
-                            }}
-                        />
-                    </div>
+                    )} */}
                 </div>
             )}
         </Modal>
