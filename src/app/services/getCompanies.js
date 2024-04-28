b33c792f59ea5afadb81699b9dc866e5f798228c
oarnal <oarnal@gmail.com>
2024-04-28 21:24:18 +0200

    Update. Implementació de funcions per recuperar dades del servidor

--- src/app/services/getCompanies.js
+++ src/app/services/getCompanies.js
@@ -1,6 +1,19 @@
-import Companies from "../mocks/COMPANY_MOCK_DATA.json";
+import Companies from '../mocks/COMPANY_MOCK_DATA.json';
+import { API_BASE_URL, EDPOINT } from '../utils/constants';
 
 export const getCompanies = () => {
     // TODO: Get data from API instead of mock file
-    return Companies;
+    const options = {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+        },
+    };
+
+    const companies = fetch(API_BASE_URL + EDPOINT.PROVIDERS, options)
+        .then((response) => response.json())
+        // .then((data) => console.log(data));
+
+    return companies;
 };
--- src/app/services/getServices.js
+++ src/app/services/getServices.js
@@ -1,5 +1,10 @@
-export const getServices = () => {
+import { getCompanies } from './getCompanies';
+
+export const getServices = async () => {
     // TODO: Get data from API instead of mock file
-    const services = ['restaurant', 'nails', 'lawyer', 'haircut']
+    // const services = ['restaurant', 'nails', 'lawyer', 'haircut']
+    const providers = await getCompanies();
+    const services = [...new Set(providers.map((provider) => provider.service_provided))];
+
     return services;
 };
