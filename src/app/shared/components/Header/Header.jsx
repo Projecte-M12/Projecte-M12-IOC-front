7d71ba903eca8488ff674ad5b62d21758e5fde97
oarnal <oarnal@gmail.com>
2024-05-02 20:53:19 +0200

    Update. Responsive menu

--- src/app/shared/components/Header/Header.jsx
+++ src/app/shared/components/Header/Header.jsx
@@ -4,10 +4,17 @@ import { Link } from 'react-router-dom';
 
 import './Header.scss';
 import { useLogin } from '../../../hooks/useLogin';
+import { useState } from 'react';
 
 export const Header = () => {
     const { isAuthenticated, user } = useAuthContext();
     const { handleLogout } = useLogin();
+
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
 
     return (
         <header className="header">
@@ -15,45 +22,53 @@ export const Header = () => {
                 <p>Header</p>
             </div>
             <div className="header__links">
-                <Link to={'/'} style={{ color: 'black' }}>
-                    Inici
-                </Link>
-                {isAuthenticated && user.is_company && (
-                    <Link to={'/company-dashboard'} style={{ color: 'black' }}>
-                        Company dashboard
+                <button className="menu-icon" onClick={toggleMenu}>
+                    ☰
+                </button>
+                <nav className={`menu ${menuOpen ? 'open' : ''}`}>
+                    <Link to={'/'} style={{ color: 'black' }}>
+                        Inici
                     </Link>
-                )}
-                {isAuthenticated && !user.is_company && (
-                    <>
-                        <Link to={'/homepage'} style={{ color: 'black' }}>
-                            Serveis
-                        </Link>
+                    {isAuthenticated && user?.company_name && (
                         <Link
-                            to={'/customer-dashboard'}
+                            to={'/company-dashboard'}
                             style={{ color: 'black' }}
                         >
-                            User dashboard
+                            Company dashboard
                         </Link>
-                    </>
-                )}
-                {isAuthenticated ? (
-                    <Link
-                        to={'/'}
-                        onClick={handleLogout}
-                        style={{ color: 'black' }}
-                    >
-                        Logout
-                    </Link>
-                ) : (
-                    <>
-                        <Link to={'/login'} style={{ color: 'black' }}>
-                            Login
+                    )}
+                    {isAuthenticated && !user?.company_name && (
+                        <>
+                            <Link to={'/homepage'} style={{ color: 'black' }}>
+                                Serveis
+                            </Link>
+                            <Link
+                                to={'/customer-dashboard'}
+                                style={{ color: 'black' }}
+                            >
+                                User dashboard
+                            </Link>
+                        </>
+                    )}
+                    {isAuthenticated ? (
+                        <Link
+                            to={'/'}
+                            onClick={handleLogout}
+                            style={{ color: 'black' }}
+                        >
+                            Logout
                         </Link>
-                        <Link to={'/signup'} style={{ color: 'black' }}>
-                            Signup
-                        </Link>
-                    </>
-                )}
+                    ) : (
+                        <>
+                            <Link to={'/login'} style={{ color: 'black' }}>
+                                Login
+                            </Link>
+                            <Link to={'/signup'} style={{ color: 'black' }}>
+                                Signup
+                            </Link>
+                        </>
+                    )}
+                </nav>
             </div>
         </header>
     );
