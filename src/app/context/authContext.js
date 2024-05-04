1f1c357b15ed910359ed8842dae3303333785875
Aleix <aleix.guix@gmail.com>
2024-05-04 09:10:58 +0200

    Update: JSDOCS

--- src/app/context/authContext.js
+++ src/app/context/authContext.js
@@ -2,6 +2,6 @@ import { createContext } from "react";
 
 /**
  * Context d'autenticació utilitzat per proporcionar informació d'autenticació a components de React.
- * @type {React.Context} Context d'autenticació
+ * @type {Context} Context d'autenticació
  */
 export const authContext = createContext(null);
